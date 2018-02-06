f31fb6e897e829fdb5a013966e079ab57c1f9cff
sk.sun <sk.sun@r-pac.com.cn>
2018-02-06 17:36:36 +0800

    update

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     # http: //localhost:8000/blog/1
     path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
-    path('<int:blog_type_pk', views.blogs_with_type, name='blogs_with_type'),
+    path('type/<int:blog_type_pk>', views.blogs_with_type, name='blogs_with_type'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,9 +14,9 @@ def blog_detail(request, blog_pk):
     return render_to_response('blog_detail.html', context)
 
 
-def blogs_with_type(request, blogs_with_type):
+def blogs_with_type(request, blog_type_pk):
     context = {}
-    blog_type = get_object_or_404(BlogType, pk=blogs_with_type)
+    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
     return render_to_response('blogs_with_type.html', context)
