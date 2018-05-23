2d9105e17d543ce2851af04c066d384b28a8fdc2
sk <2498256234@qq.com>
2018-05-23 11:05:33 +0800

    add disqus

--- blog/admin.py
+++ blog/admin.py
@@ -12,11 +12,15 @@ class BlogTypeAdmin(admin.ModelAdmin):
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'blog_type', 'author', 'get_read_num', 'created_time', 'updated_time', 'tags_list']
     filter_horizontal = ('tags',)
+    list_display_links = ('id', 'title')
+    list_filter = ('tags', 'created_time')  # 过滤器
+    search_fields = ('title',)  # 搜索字段
 
 
 @admin.register(Tag)
 class BlogTagAdmin(admin.ModelAdmin):
     list_display = ['id', 'name']
+    list_display_links = ('id', 'name')
 
 
 
